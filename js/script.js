ec9d31aa2a1a7657a7503fcf1caea78d05043d45
Akshay <ariseak13@gmail.com>
2025-03-29 16:34:04 +0530

    basic ui and js done

---
+++ js/script.js
@@ -0,0 +1,40 @@
+console.log("Sup mfs")
+async function getsongs() {
+    let a=await fetch("http://127.0.0.1:3000/songs")
+    let response= await a.text();
+    let div=document.createElement("div")
+    div.innerHTML=response;
+    let as=div.getElementsByTagName("a")
+    let songs=[]
+    for (let index = 0; index < as.length; index++) {
+        const element = as[index];
+        if(element.href.endsWith(".mp3")){
+            songs.push(element.href.split("/songs/")[1])
+        }
+        
+    }
+    return songs
+}
+
+async function main() {
+    let songs= await getsongs()
+    console.log(songs)
+
+    let songUL= document.querySelector(".songlist").getElementsByTagName("ul")[0]
+    for (const song of songs) {
+        songUL.innerHTML = songUL.innerHTML + `<li><img class="invert" width="34" src="/images/music.svg" alt="">
+                            <div class="info">
+                                <div> ${song.replaceAll("%20", " ")}</div>
+                            </div>
+                            <div class="playnow">
+                                <span>Play Now</span>
+                                <img class="invert" src="/images/play.svg" alt="">
+                            </div> </li>`;
+    }
+
+    // var audio = new Audio(songs[0]);
+    // audio.play();
+    
+}
+
+main()
