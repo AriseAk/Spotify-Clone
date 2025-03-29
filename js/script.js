c6f3293c4daeeb808ed46c16be6cfadbc05f67b8
Akshay <ariseak13@gmail.com>
2025-03-29 18:06:15 +0530

    play song feature added

--- js/script.js
+++ js/script.js
@@ -1,26 +1,39 @@
 console.log("Sup mfs")
+
+let currentsong = new Audio();
+
 async function getsongs() {
-    let a=await fetch("http://127.0.0.1:3000/songs")
-    let response= await a.text();
-    let div=document.createElement("div")
-    div.innerHTML=response;
-    let as=div.getElementsByTagName("a")
-    let songs=[]
+    let a = await fetch("http://127.0.0.1:3000/songs")
+    let response = await a.text();
+    let div = document.createElement("div")
+    div.innerHTML = response;
+    let as = div.getElementsByTagName("a")
+    let songs = []
     for (let index = 0; index < as.length; index++) {
         const element = as[index];
-        if(element.href.endsWith(".mp3")){
+        if (element.href.endsWith(".mp3")) {
             songs.push(element.href.split("/songs/")[1])
         }
-        
+
     }
     return songs
 }
 
+const playsongs = (track) => {
+    currentsong.src = "/songs/" + track
+    currentsong.play()
+    play.src="/images/pause.svg"
+    document.querySelector(".songinfo").innerHTML=track    
+    document.querySelector(".songtime").innerHTML="00.00/00.00"    
+}
+
+
 async function main() {
-    let songs= await getsongs()
+
+    let songs = await getsongs()
     console.log(songs)
 
-    let songUL= document.querySelector(".songlist").getElementsByTagName("ul")[0]
+    let songUL = document.querySelector(".songlist").getElementsByTagName("ul")[0]
     for (const song of songs) {
         songUL.innerHTML = songUL.innerHTML + `<li><img class="invert" width="34" src="/images/music.svg" alt="">
                             <div class="info">
@@ -32,9 +45,25 @@ async function main() {
                             </div> </li>`;
     }
 
-    // var audio = new Audio(songs[0]);
-    // audio.play();
-    
+    Array.from(document.querySelector(".songlist").getElementsByTagName("li")).forEach(e => {
+        e.addEventListener("click", element => {
+            console.log(e.querySelector(".info").firstElementChild.innerHTML)
+            playsongs(e.querySelector(".info").firstElementChild.innerHTML.trim())
+        })
+
+    })
+
+    play.addEventListener("click",()=>{
+        if(currentsong.paused){
+            currentsong.play()
+            play.src="/images/pause.svg"
+        }
+        else{
+            currentsong.pause()
+            play.src="/images/play.svg"
+        }
+
+    })
 }
 
 main()
